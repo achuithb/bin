5b5e755083a49ffbc5febc4f4efc5a72a14c69c5
Achuith Bhandarkar <achuith@chromium.org>
2017-10-27 11:07:34 +0200

    [git_update] Determine path from cwd instead of specifying as an arg.

--- git_update.py
+++ git_update.py
@@ -4,48 +4,44 @@ import os, sys
 
 import git_lib
 
-def Prepare(path):
-  os.chdir(path)
-  git_lib.GitCheckoutMaster()
-
-
-def Pull(path):
+def Pull():
   git_lib.GitPull()
 
 
-def Rebase(path):
+def Rebase():
   git_lib.GitRebaseAll()
 
 
-def Sync(path):
-  if (path != git_lib.CHROME_DIR):
+def Sync():
+  if (os.getcwd() != git_lib.CHROME_DIR):
     return
   os.chdir(os.path.dirname(git_lib.CHROME_DIR))
   git_lib.RunCmd('gclient sync -j16')
+  os.chdir(os.path.dirname(git_lib.CHROME_DIR))
 
 
-def All(path):
-  Pull(path)
-  Rebase(path)
-  Sync(path)
+def All():
+  Pull()
+  Sync()
+  Rebase()
 
 
 def main(argv):
   repo_map = { 'chrome' : git_lib.CHROME_DIR, 'catapult': git_lib.CATAPULT_DIR }
   func_map = { 'pull' : Pull, 'rebase' : Rebase, 'sync': Sync, 'all': All }
 
-  if (len(argv) != 3 or
-      argv[1] not in repo_map.keys() or
-      argv[2] not in func_map.keys()):
-    func_str = '|'.join(func_map.keys())
-    repo_str = '|'.join(repo_map.keys())
-    print "Usage: %s [%s] [%s]" % (os.path.basename(argv[0]),
-                                   repo_str, func_str)
+  if (len(argv) != 2 or
+      argv[1] not in func_map.keys()):
+    print 'Usage: %s [%s]' % (os.path.basename(argv[0]),
+                              '|'.join(func_map.keys()))
     sys.exit(1)
 
-  path = repo_map[argv[1]]
-  Prepare(path)
-  func_map[argv[2]](path)
+  if (os.getcwd() not in [git_lib.CHROME_DIR, git_lib.CATAPULT_DIR]):
+    print '%s is not a valid cwd for this command' % os.getcwd()
+    sys.exit(1)
+
+  git_lib.GitCheckoutMaster()
+  func_map[argv[1]]()
 
 
 if __name__ == '__main__':
