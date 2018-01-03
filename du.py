8ee8f9f84375bf9d7f3ca892d8c9bf942f90a886
Achuith Bhandarkar <achuith@chromium.org>
2018-01-02 16:52:03 -0800

    [du]: Handle var/cache by default.

--- du.py
+++ du.py
@@ -2,56 +2,74 @@
 
 import os, sys, subprocess
 
-#TODO(achuith): Use argparse for these.
-MIN_SIZE = 10  # Skip anything less than 10G.
-MAX_DEPTH = 7
-HOME = '/usr/local/google/home/achuith'
+import utils
 
-def CheckOutput(args):
-  #print args
-  return subprocess.check_output(args).rstrip()
+#TODO: Use argparse for these.
+DEFAULT_DEPTH = 7
+CACHE_DEPTH = 4
+MIN_SIZE = 10  # Skip anything less than 10G.
+
+HOME = '/usr/local/google/home/achuith'
+CODE = os.path.join(HOME, 'code')
+CACHE = os.path.join(HOME, 'code/cros/chroot/var/cache')
+
 
 def PrintUsageForDir(ary):
   print '%s: %dG' % (ary[1], ary[0])
 
+
 def DiskUsage(d):
-  ary = subprocess.check_output(['sudo', 'du', '-s', d]).rstrip().split()
+  ary = utils.RunCmd(['sudo', 'du', '-s', d], silent=True).rstrip().split()
   ary[0] = int(ary[0])
   #print ary
   return ary  # (numblocks, name)
 
-def PrintTotalUsage(cols):
+
+def PrintTotalUsage(cols, depth):
   for row in sorted(cols, reverse=True, key=lambda a: a[0]):
     row[0] = row[0]/(1024 * 1024)
     if row[0] < MIN_SIZE:
       break;
     PrintUsageForDir(row)
-    Recurse(row[1])
+    Recurse(row[1], depth)
 
-def Recurse(root):
-  if root.count('/') >= MAX_DEPTH:
+
+def Recurse(root, depth):
+  if not depth:
     return
   dirs = os.listdir(root)
   for i in range(len(dirs)):
     dirs[i] = os.path.join(root, dirs[i])
   #print dirs
-  UsageForDirs(dirs)
+  UsageForDirs(dirs, depth-1)
 
-def UsageForDirs(dirs):
+
+def UsageForDirs(dirs, depth):
   cols = []
   for d in dirs:
     # Skip non-directories.
     if os.path.isdir(d):
       cols.append(DiskUsage(d))
-  PrintTotalUsage(cols)
+  PrintTotalUsage(cols, depth)
+
+
+def DefaultDir(path, depth):
+  os.chdir(os.path.dirname(path))
+  UsageForDirs([os.path.basename(path)], depth)
+
+
+def DefaultDirs():
+  DefaultDir(CODE, DEFAULT_DEPTH)
+  print '\n%s' % CACHE
+  DefaultDir(CACHE, CACHE_DEPTH)
 
 
 def main(argv):
   dirs = argv[1:]
-  if not dirs:
-    os.chdir(HOME)
-    dirs = ['code']
-  UsageForDirs(dirs)
+  if dirs:
+    UsageForDirs(dirs, DEFAULT_DEPTH)
+  else:
+    DefaultDirs()
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv))
