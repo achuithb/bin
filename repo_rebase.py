705caa285650a2978681e25d03aab43d8660cfd7
Achuith Bhandarkar <achuith@chromium.org>
2018-07-20 13:28:38 -0700

    [repo_rebase]: Support for final_branch

--- repo_rebase.py
+++ repo_rebase.py
@@ -5,16 +5,18 @@ import sys
 import git_lib
 import utils
 
-def RepoRebase(skip_list=None):
+def RepoRebase(skip_list=None, final_branch=None):
   if not skip_list:
     skip_list = []
   for branch in git_lib.GitListBranches():
     if branch not in skip_list:
       git_lib.GitCheckout(branch)
       utils.RunCmd('repo rebase .')
+  if final_branch:
+    git_lib.GitCheckout(final_branch)
 
 def main(argv):
-  RepoRebase(['old_refactor', 'qemu_version_retry'])
+  RepoRebase(['old_refactor', 'qemu_version_retry'], 'unittest')
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv))
