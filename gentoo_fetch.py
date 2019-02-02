7505b1e4d25ef1969acb8e1179884f0482542190
Achuith Bhandarkar <achuith@chromium.org>
2019-02-01 17:23:06 -0800

    [gentoo_fetch]: Break into functions

--- gentoo_fetch.py
+++ gentoo_fetch.py
@@ -15,22 +15,41 @@ DEFAULT_FILES = ['Manifest', 'metadata.xml']
 
 def ParseArgs(argv):
   parser = argparse.ArgumentParser('Script to fetch Gentoo Package files')
-  parser.add_argument('--package', help='Name of package')
   parser.add_argument('--ebuild', help='Name of ebuild')
   parser.add_argument('--patches', nargs='*', help='Patch files.')
   return parser.parse_args(argv[1:])
 
-def main(argv):
-  opts = ParseArgs(argv)
-  files = DEFAULT_FILES + [opts.ebuild]
-  for patch in opts.patches:
+
+def MkDir(patches):
+  if patches:
+    try:
+      os.mkdir('files')
+    except OSError:
+      pass
+
+
+def CreateFileList(ebuild, patches):
+  files = DEFAULT_FILES + [ebuild]
+  for patch in patches:
     files += ['files/%s' % patch]
-  if len(PATCHES):
-    os.mkdir('files')
+  return files
+
+
+def FetchFiles(files):
+  cwd = os.getcwd()
+  root = os.path.dirname(os.path.dirname(cwd))
+  package = os.path.relpath(cwd, root)
   for filename in files:
-    http_path = HTTP_PATH % (opts.package, filename)
+    http_path = HTTP_PATH % (package, filename)
     with open(filename, 'w') as fd:
       fd.write(utils.RunCmd('curl %s' % http_path))
+
+
+def main(argv):
+  opts = ParseArgs(argv)
+  MkDir(opts.patches)
+  FetchFiles(CreateFileList(opts.ebuild, opts.patches))
+
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv))
