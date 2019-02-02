915280fc0ec6fa8db0cd1ecd7a13673e7de6224a
Achuith Bhandarkar <achuith@chromium.org>
2019-02-01 17:06:16 -0800

    [gentoo_fetch]: Update to use argparse.

--- gentoo_fetch.py
+++ gentoo_fetch.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python
 
+import argparse
 import os
 import sys
 
 import utils
 
-EBUILD = 'blas-reference-20070226-r4.ebuild'
-PACKAGE = 'sci-libs/blas-reference'
 PATCHES = [
-  'blas-reference-20070226-autotool.patch',
-  'blas-reference-20070226-pkg-config.patch',
-  'eselect.blas.reference',
-  'eselect.blas.reference-r1',
 ]
-GIT_ID = '4739b0a41d6e1a1e827f8df9384ee2d4d4ab5466'
-HTTP_PATH = 'https://gitweb.gentoo.org/repo/gentoo.git/plain/%s/%s?id=%s'
+HTTP_PATH = ('https://gitweb.gentoo.org/repo/gentoo.git/plain/%s/%s?id='
+             '4739b0a41d6e1a1e827f8df9384ee2d4d4ab5466')
+
 DEFAULT_FILES = ['Manifest', 'metadata.xml']
 
+def ParseArgs(argv):
+  parser = argparse.ArgumentParser('Script to fetch Gentoo Package files')
+  parser.add_argument('--package', help='Name of package')
+  parser.add_argument('--ebuild', help='Name of ebuild')
+  parser.add_argument('--patches', nargs='*', help='Patch files.')
+  return parser.parse_args(argv[1:])
+
 def main(argv):
-  files = DEFAULT_FILES + [EBUILD]
-  for patch in PATCHES:
+  opts = ParseArgs(argv)
+  files = DEFAULT_FILES + [opts.ebuild]
+  for patch in opts.patches:
     files += ['files/%s' % patch]
   if len(PATCHES):
     os.mkdir('files')
   for filename in files:
-    http_path = HTTP_PATH % (PACKAGE, filename, GIT_ID)
+    http_path = HTTP_PATH % (opts.package, filename)
     with open(filename, 'w') as fd:
       fd.write(utils.RunCmd('curl %s' % http_path))
 
