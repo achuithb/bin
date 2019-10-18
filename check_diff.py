55194d558656d48b2f51d54f1f80d012fd153593
Achuith Bhandarkar <achuith@chromium.org>
2019-10-18 16:59:18 -0700

    [check_diff]: New files to test.

--- check_diff.py
+++ check_diff.py
@@ -4,26 +4,54 @@ import os, sys
 
 import utils
 
-FILES = ['android_build.py',
-         'artifact_info.py',
-         'build_artifact.py',
-         'build_util.py',
-         'common_util.py',
-         'devserver_constants.py',
-         'downloader.py',
-         'log_util.py',
-         'retry.py',
-         'xbuddy.py',
-         ]
+
+FILES = [
+    '__init__.py',
+    'nebraska.py',
+    'nebraska_unittest.py',
+    'README.md',
+    'sample.json',
+]
+
+FILES = [
+    'android_build.py',
+    'artifact_info.py',
+    'build_artifact.py',
+    'build_util.py',
+    'common_util.py',
+    'devserver_constants.py',
+    'downloader.py',
+    'log_util.py',
+    'retry.py',
+    'xbuddy.py',
+]
+
+FILES = [
+    'android_build.py',
+    'artifact_info.py',
+    'build_artifact.py',
+    'build_artifact_unittest.py',
+    'build_util.py',
+    'common_util.py',
+    'common_util_unittest.py',
+    'devserver_constants.py',
+    'downloader.py',
+    'downloader_unittest.py',
+    'log_util.py',
+]
+
+
+PLATFORM = 'src/platform/dev'
+CHROMITE = 'chromite/lib'
 
 def main(argv):
   for f in FILES:
-    plat = os.path.join('src/platform/dev', f)
-    chromite = os.path.join('chromite/lib', f)
+    platform = os.path.join(utils.CROS_DIR, PLATFORM, f)
+    chromite = os.path.join(utils.CROS_DIR, CHROMITE, f)
     try:
-      utils.RunCmd(['/usr/bin/diff', plat, chromite], silent=True)
+      utils.RunCmd(['/usr/bin/diff', platform, chromite], silent=True)
     except Exception as e:
-      print 'diff %s %s' % (plat, chromite)
+      print(f)
 
 
 if __name__ == '__main__':
