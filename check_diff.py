fb365d7872f0f27ef2f8e218812d4de2bf65470e
Achuith Bhandarkar <achuith@chromium.org>
2019-11-01 11:41:52 -0700

    [check_diff]: Generalize

--- check_diff.py
+++ check_diff.py
@@ -22,20 +22,32 @@ FILES = [
     'xbuddy_unittest.py',
 ]
 
+FILES = [
+    'devserver_integration_test.py',
+    'devserver.py',
+    'health_checker.py',
+    'autoupdate.py',
+    'autoupdate_unittest.py',
+    'cros_update_progress.py',
+]
+
 # android_build.py artifact_info.py build_artifact.py build_artifact_unittest.py build_util.py common_util.py common_util_unittest.py devserver_constants.py downloader.py downloader_unittest.py log_util.py retry.py xbuddy.py xbuddy_unittest.py
 
+# devserver_integration_test.py devserver.py health_checker.py autoupdate.py autoupdate_unittest.py cros_update_progress.py
 
-PLATFORM = 'src/platform/dev/'
-CHROMITE = 'chromite/lib/xbuddy/'
+DIR1 = utils.AbsPath('code/cros/src/platform/dev/')
+DIR2 = utils.AbsPath('code/cros/chromite/lib/xbuddy/')
+DIR2 = utils.AbsPath('code/misc/xbuddy')
 
 def main(argv):
   for f in FILES:
-    platform = os.path.join(utils.CROS_DIR, PLATFORM, f)
-    chromite = os.path.join(utils.CROS_DIR, CHROMITE, f)
+    path1 = os.path.join(DIR1, f)
+    path2 = os.path.join(DIR2, f)
     try:
-      utils.RunCmd(['/usr/bin/diff', platform, chromite], silent=True)
+      utils.RunCmd(['/usr/bin/diff', path1, path2], silent=True)
     except Exception as e:
-      print(f)
+      utils.ColorPrint(utils.MAGENTA, f)
+      print(e.output)
 
 
 if __name__ == '__main__':
